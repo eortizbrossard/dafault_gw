ab3d83abf570a30a4097ba1a38f30ba7dd492f8a
eortizbrossard <brossard.emiliano@gmail.com>
2019-09-24 16:47:35 -0300

    Update default_gw.sh

--- default_gw.sh
+++ default_gw.sh
@@ -1,22 +1,38 @@
 #!/bin/bash -       
-#title           : default_gw.sh
+#title           :default_gw.sh
 #description     : Add a static route as default gateway in the .1 address of the network segment configured in the interface. 
-#author		       : eob
-#date            : 20190923
-#version         : 0.1
-#usage		       : bash defautl_gw.sh
-#notes           : Modify interface if necessary
+#author		     : eob
+#date            :20190923
+#version         :0.1
+#usage		     :bash defautl_gw.sh
+#notes           :Modify interface if necessary
 #bash_version    :
 #==============================================================================
-# Stores the interface ip in variable 
-eth_ip=$(ifdata -pa eth0)
 
-# Separates the first three octets from the IP address and the address .1 is stored in its variable.
-ip_range=$(echo $eth_ip | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.')
-gw_ip=$(echo $ip_range'1')
+change_gw () {
 
-# Checks if there is a correct static path for the interface. If it doesn't exist, configure the address stored in the variable gw_ip
-fail=$(route -n | grep -q $gw_ip; echo $?)
-if [ "$fail" -ne 0 ]; then
-	route add default gw $gw_ip wlan0
+    # Stores the interface ip in variable 
+    eth_ip=$(ifdata -pa $iface)
+
+    # Separates the first three octets from the IP address and the address .1 is stored in its variable.
+    ip_range=$(echo $eth_ip | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.')
+    gw_ip=$(echo $ip_range'1')
+
+    # Checks if there is a correct static path for the interface. If it doesn't exist, configure the address stored in the variable gw_ip
+    fail=$(route -n | grep -q $gw_ip; echo $?)
+    if [ "$fail" -ne 0 ]; then
+    	route add default gw $gw_ip wlan0
+    fi
+}
+
+iface="$1"
+if [ ! -n "$1" ]
+then
+    echo "$0 no interface parameter, setting eth0 as default"
+	iface=$(echo 'eth0')
+	change_gw
+else
+	echo "interface $1 set and now starting $0 shell script..."
+	change_gw
 fi
+    
